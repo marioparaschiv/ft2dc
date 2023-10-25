db19763e003467f07d92fd7d3de0a31b2fe7350c
eternal <eternal.git@gmail.com>
2023-10-25 19:08:54 +0100

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import { createLogger } from '@lib/logger';
 import { Routes, URL } from '@constants';
+import { sleep } from '@utilities';
 import config from '@config';
 
 class API {
@@ -7,29 +8,36 @@ class API {
 	chats = [];
 
 	async getPortfolio() {
-		const res = await fetch(URL.API + Routes.Portfolio(config.portfolio), {
-			headers: {
-				'Authorization': config.auth
+		try {
+			const res = await fetch(URL.API + Routes.Portfolio(config.portfolio), {
+				headers: {
+					'Authorization': config.auth
+				}
+			});
+
+			if (!res?.ok) {
+				this.logger.error('Failed to retrieve profile information. Retrying in 500ms.', { res });
+				await sleep(500);
+				return this.getPortfolio();
 			}
-		}).catch(() => null);
 
-		if (!res?.ok) {
-			this.logger.error('Failed to retrieve profile information.', { res });
-			return {};
+			return await res.json();
+		} catch (error) {
+			this.logger.error('Failed to retrieve profile information. Retrying in 500ms.', { error });
+			await sleep(500);
+			return this.getPortfolio();
 		}
-
-		return await res.json();
 	}
 
 	async getChats(): Promise<Chat[]> {
 		const portfolio = await this.getPortfolio();
-		this.logger.debug(portfolio);
+		if (!portfolio) return this.chats;
+
 		const chats = portfolio.holdings;
 		this.logger.debug('Holdings:', chats);
 
 		if (!chats) this.logger.warn('!!! GOT NO HOLDINGS !!! WTF???');
-
-		this.chats = chats;
+		if (chats) this.chats = chats;
 		this.logger.success('Chats successfully updated.');
 
 		return this.chats;
